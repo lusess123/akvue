2309fca632d5132e4be7f1442501957daf29cbc7
zyking <zyking@zykingdeMacBook-Pro.local>
2018-04-10 13:06:26 +0800

    test

--- packages/ak-dev-node/src/util/fileutil.spec.ts
+++ packages/ak-dev-node/src/util/fileutil.spec.ts
@@ -1,10 +1,14 @@
 import  * as ff from "./fileutil"
+import * as path from "path"
 
 
-describe("aaa",()=>{
+describe("fileutil 文件操作工具类",()=>{
 
-    it('ggg', () => {
-        console.log(ff.getStringByFileName(__filename));
-         expect(  ff.ggg(123)).toBe(123);
-    });
+    it('文件文本读取', () => {
+        
+        const _path =   path.resolve("./src/test/fixtures/file.json");
+        const _res =   ff.getStringByFileName(_path);
+        console.log(_res);
+         expect(  _res.trim()).toBe("123");
+    }); 
 });
