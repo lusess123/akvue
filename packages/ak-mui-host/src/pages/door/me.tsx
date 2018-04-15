bc1e879193b42b0b4bfbe91a5af12d0bd0bb303d
zyking　 <zyking@zykingdeMacBook-Pro-2.local>
2018-04-15 23:57:14 +0800

    me

--- packages/ak-mui-host/src/pages/door/me.tsx
+++ packages/ak-mui-host/src/pages/door/me.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React  from 'react';
+import PropTypes from 'prop-types';
 import Button from "antd/lib/button"
 import { connect } from 'dva';
-import  * as sys from "ak-lib-sys"
+import * as sys from "ak-lib-sys"
 import { DemoVm } from "./models/door"
 
-const pp = 
+
+const Child1:React.SFC<any> = (p) => {
+    return <div>
+        <pre><code>{sys.core.json({ ...p })}</code></pre>
+
+    </div>
+}
+
+export const Gg:any = ()=>{
+     return <div></div>
+}
+
+const pp =
     (props) => {
         const { door } = props;
         // debugger;
         return <div>
+           
             <pre><code>{sys.core.json(
                 {
                     ...props,
@@ -16,9 +30,9 @@ const pp =
                         match: undefined,
                         history: undefined,
                         location: undefined,
-                        route:undefined,
-                        routing:undefined,
-                        children:undefined
+                        route: undefined,
+                        routing: undefined,
+                        children: undefined
 
                     }
                 })}</code></pre>
@@ -26,6 +40,10 @@ const pp =
             <pre><code>{sys.core.json(door.Vm)}</code></pre>
 
             <Button>点击我</Button>
+            <Child1 {...props} />
+            <hr/>
+            <h1>子组件</h1>
+            <AA/>
         </div>
     }
 
@@ -33,6 +51,28 @@ const pp =
 export default connect((p) => {
     return { ...p }
 })(pp);
+
+const  Vvv = connect()(Gg);
+
+
+const Child = connect(
+    (p) => {
+        return { 
+           // ...p, 
+            xxx: p.door 
+        }
+    }
+    )(Child1);
+
+
+    export  class AA extends React.Component<any,any> {
+       static contextTypes = { store: PropTypes.object.isRequired }
+          render(){
+              debugger;
+              const _ff = this.context.store.getState();
+              return <div><Child1  {..._ff}   /></div>
+          }
+    }
 
 
 
