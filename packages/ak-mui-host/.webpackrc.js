ea4651d4768d51b6f97e690763f24ec74eda56ea
zyking <zyking@zykingdeMacBook-Pro.local>
2018-04-19 21:53:56 +0800

    s

--- packages/ak-mui-host/.webpackrc.js
+++ packages/ak-mui-host/.webpackrc.js
@@ -1,19 +1,24 @@
-import { resolve } from 'path';
+import {resolve} from 'path';
 
 export default {
-  alias: {
-    components: resolve(__dirname, './components'),
+  alias : {
+    components: resolve(__dirname, './components')
   },
-  extraBabelIncludes:[
-        './node_modules/ddd/index.tsx',
-        './../ak-lib-antd/src/pro/TagSelect/index.jsx'
+  extraBabelIncludes : [
+    './node_modules/ddd/index.tsx', './../ak-lib-antd/src/pro/TagSelect/index.jsx'
   ],
-  
-  extraBabelPlugins: [
-   
-    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }]
+
+  extraBabelPlugins : [
+
+    [
+      'import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true
+      }
+    ]
   ],
-  "theme": {
+  "theme" : {
     "primary-color": "#1bab5b"
-    }
+  }
 };
