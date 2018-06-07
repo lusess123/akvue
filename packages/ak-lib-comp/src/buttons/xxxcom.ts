cdc4d91c1e6f82210290f494130b27fca458e1a0
郑瑜琨 <zhengyukun@lvwan.com>
2018-06-07 11:14:26 +0800

    comp 小写

---
+++ packages/ak-lib-comp/src/buttons/xxxcom.ts
@@ -0,0 +1,21 @@
+import {BaseCom,IBaseComConfig} from "ak-lib-sys/com/BaseCom";
+import Vue from "vue";
+import { core ,ioc,vue } from "ak-lib-sys";
+
+
+
+export interface IXXXComConfig extends IBaseComConfig{
+   
+}
+
+
+@ioc.PlugIn({RegName:"XXXCol",BaseType:"BaseCom",Author:"zhengyukun",CreateDate:"2018-01-26",Doc:"XXX组件插件"})
+@vue.com('<div>XXXCom</div>')
+export   class XXXCom extends BaseCom {
+
+      constructor (config?: IXXXComConfig){
+          super(config);
+      }
+
+
+}
